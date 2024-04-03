479162ab91ddbf03fd3bb0076243008c75291013
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-04-03 12:43:50 +0200

    button

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -8,7 +8,8 @@ const [likes, setLikes]= useState(0);
 const handleClick = () => { setLikes(likes + 1) }
     return (
 
-        <button type="button" className="like-button" onClick={handleClick} >
+
+<button type="button" className="like-button" onClick={handleClick} >
           {likes} Likes
         </button>
 
